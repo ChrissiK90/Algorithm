e6bc20e74f6767a6ffa6864a75b7addc0d8e3ec1
s54824 <christina.koren90@gmail.com>
2024-06-21 11:50:52 +0200

    bla

--- src/UE02_AddAndSearch/Node.java
+++ src/UE02_AddAndSearch/Node.java
@@ -3,48 +3,46 @@ package UE02_AddAndSearch;
 public class Node
 {
     // *** Membervariablen ***
-    private int number;
-    private  Node left;
-    private Node right;
-    private Node parent;
+    private int number; // Wert des Knotens
+    private Node left; // linker Knoten
+    private Node right; // rechter Knoten
+    private Node parent; // Elternknoten
 
     // *** Konstruktor ***
-
     public Node(int number) {
-        this.number = number;
+        this.number = number; // Wert setzen
     }
 
     // *** Getter- & Setter-Methoden ***
-
     public int getNumber() {
-        return number;
+        return number; // Wert zurückgeben
     }
 
     public void setNumber(int number) {
-        this.number = number;
+        this.number = number; // Wert setzen
     }
 
     public Node getLeft() {
-        return left;
+        return left; // linken Knoten zurückgeben
     }
 
     public void setLeft(Node left) {
-        this.left = left;
+        this.left = left; // linken Knoten setzen
     }
 
     public Node getRight() {
-        return right;
+        return right; // rechten Knoten zurückgeben
     }
 
     public void setRight(Node right) {
-        this.right = right;
+        this.right = right; // rechten Knoten setzen
     }
 
     public Node getParent() {
-        return parent;
+        return parent; // Elternknoten zurückgeben
     }
 
     public void setParent(Node parent) {
-        this.parent = parent;
+        this.parent = parent; // Elternknoten setzen
     }
 }
--- src/UE02_AddAndSearch/Tree.java
+++ src/UE02_AddAndSearch/Tree.java
@@ -2,156 +2,149 @@ package UE02_AddAndSearch;
 
 public class Tree {
 
-    //zeigt auf die Referenz Wurzelknoten
+    // zeigt auf die Referenz Wurzelknoten
     private Node root;
 
+    // Methode zum Hinzufügen einer Zahl zum Baum
     public void add(int number) {
-        //Knoten erstellen
+        // Knoten erstellen
         Node newNode = new Node(number);
 
-        //VARIANTE LOOP
-        //addLoop(newNode);
+        // VARIANTE LOOP
+        // addLoop(newNode);
 
-        //VARIANTE REKURSION
-        //Fall: Baum noch leer:
+        // VARIANTE REKURSION
+        // Fall: Baum noch leer
         if (root == null) {
-            root = newNode;
+            root = newNode; // Wurzelknoten setzen
         }
-        //wenn nicht leer:
+        // wenn nicht leer
         else {
-            addRecursive(root, newNode);
+            addRecursive(root, newNode); // neuen Knoten rekursiv hinzufügen
         }
     }
 
     // Add - Variante Rekursion
     private void addRecursive(Node currentNode, Node newNode) {
-
-        //entweder so oder so, aufpassen beim schreiben!!!
-        //if (currentNode.getNumber() > newNode.getNumber())
-        //neuer wert kleiner:
+        // neuer Wert ist kleiner als aktueller Knotenwert
         if (newNode.getNumber() < currentNode.getNumber()) {
-            //fall: links ist schon ein Knoten
+            // Fall: links ist schon ein Knoten
             if (currentNode.getLeft() != null) {
-                //Rekursion mit linkem Knoten weiterführen
+                // Rekursion mit linkem Knoten weiterführen
                 addRecursive(currentNode.getLeft(), newNode);
             } else {
+                // neuen Knoten links anhängen
                 currentNode.setLeft(newNode);
-                newNode.setParent(currentNode);
+                newNode.setParent(currentNode); // Elternknoten setzen
             }
-        }//neuer wert größer
+        }
+        // neuer Wert ist größer oder gleich dem aktuellen Knotenwert
         else {
+            // Fall: rechts ist schon ein Knoten
             if (currentNode.getRight() != null) {
-                //Rekursion mit rechtem Knoten weiterführen
+                // Rekursion mit rechtem Knoten weiterführen
                 addRecursive(currentNode.getRight(), newNode);
             } else {
+                // neuen Knoten rechts anhängen
                 currentNode.setRight(newNode);
-                newNode.setParent(currentNode);
+                newNode.setParent(currentNode); // Elternknoten setzen
             }
         }
     }
 
     // Add - Variante Loop
     private void addLoop(Node newNode) {
-        // Fall: Baum ist leer:
+        // Fall: Baum ist leer
         if (root == null) {
-            root = newNode;
+            root = newNode; // Wurzelknoten setzen
         }
-        //wenn nicht leer ist (mind 1 Rootnode):
+        // wenn nicht leer (mindestens 1 Wurzelknoten)
         else {
-            //Hilfsvariable
+            // Hilfsvariable
             Node current = root;
-            while (true){
-                //Neuer Wert ist kleiner:
+            while (true) {
+                // Neuer Wert ist kleiner als aktueller Knotenwert
                 if (newNode.getNumber() < current.getNumber()) {
-
-                    //Wenn links schon ein Knoten ist, dann weiterhüpfen
+                    // Wenn links schon ein Knoten ist, dann weiterhüpfen
                     if (current.getLeft() != null) {
                         current = current.getLeft();
-                        //Wenn links kein Knoten ist, dann dort anhängen
                     } else {
+                        // Wenn links kein Knoten ist, dann dort anhängen
                         current.setLeft(newNode);
-                        newNode.setParent(current); //parent setzen
+                        newNode.setParent(current); // Elternknoten setzen
                         break;
                     }
                 }
-                    //Neuer Wert ist größer
-                else{
-                        if (current.getRight() != null) {
-                            current = current.getRight();
-                        } else {
-                            current.setRight(newNode);
-                            newNode.setParent(newNode); // parent setzen
-                            newNode.setParent(newNode); // parent setzen
-                            break;
-                        }
+                // Neuer Wert ist größer oder gleich dem aktuellen Knotenwert
+                else {
+                    if (current.getRight() != null) {
+                        current = current.getRight();
+                    } else {
+                        current.setRight(newNode);
+                        newNode.setParent(current); // Elternknoten setzen
+                        break;
                     }
                 }
             }
         }
-
-        public Node searchNumber ( int number){
-            System.out.print("SearchNumber - ");
-
-            // TODO: Entweder Variante 1 oder 2 einkommentieren
-            // Variante 1: Aufruf Rekursiv
-            //return searchNumberRecursive(root, number);
-            // Variante 2: Aufruf Schleife
-            return searchNumberLoop(number);
-        }
-
-        // Suche - Variante Rekursion
-        private Node searchNumberRecursive (Node currentNode,int number){
-
-            if (currentNode.getNumber() == number) {
-                return currentNode;
-            }
-
-            if (number < currentNode.getNumber()) {
-                return searchNumberRecursive(currentNode.getLeft(), number);
-            }
-            else {
-                return searchNumberRecursive(currentNode.getRight(), number);
-            }
-        }
-
-
-        // Suche - Variante Schleife
-        private Node searchNumberLoop ( int number){
-
-            Node current = root;
-
-            while (current != null && current.getNumber() != number) {
-                if (number < current.getNumber()) {
-                    current = current.getLeft();
-
-                } else {
-                    current = current.getRight();
-                }
-            }
-            return current;
-        }
-
-        /**
-         * Funktion zur Ausgabe des gesamten Baums.
-         */
-        public void printTree () {
-            printTree(root, "");
-        }
-
-        /**
-         * Funktion zur Ausgabe des Baums unterhalb eines Knotens
-         *
-         * @param currentNode Knoten, dessen Teilbaum ausgegeben werden soll
-         * @param prefix      Zur Einr�ckung
-         */
-        public void printTree (Node currentNode, String prefix){
-            if (currentNode == null) {
-                return;
-            }
-            System.out.println(prefix + currentNode.getNumber());
-            printTree(currentNode.getLeft(), prefix + "L ");
-            printTree(currentNode.getRight(), prefix + "R ");
-        }
-
-
     }
+
+    // Methode zum Suchen einer Zahl im Baum
+    public Node searchNumber(int number) {
+        System.out.print("SearchNumber - ");
+
+        // Variante 1: Aufruf Rekursiv
+        // return searchNumberRecursive(root, number);
+
+        // Variante 2: Aufruf Schleife
+        return searchNumberLoop(number);
+    }
+
+    // Suche - Variante Rekursion
+    private Node searchNumberRecursive(Node currentNode, int number) {
+        // Basisfall: Knoten ist null oder Wert gefunden
+        if (currentNode == null || currentNode.getNumber() == number) {
+            return currentNode; // Knoten zurückgeben
+        }
+        // Wert ist kleiner, links weiter suchen
+        if (number < currentNode.getNumber()) {
+            return searchNumberRecursive(currentNode.getLeft(), number);
+        }
+        // Wert ist größer, rechts weiter suchen
+        else {
+            return searchNumberRecursive(currentNode.getRight(), number);
+        }
+    }
+
+    // Suche - Variante Schleife
+    private Node searchNumberLoop(int number) {
+        Node current = root;
+        // Schleife durchlaufen bis der Knoten gefunden wird oder null ist
+        while (current != null && current.getNumber() != number) {
+            if (number < current.getNumber()) {
+                current = current.getLeft();
+            } else {
+                current = current.getRight();
+            }
+        }
+        return current; // Knoten zurückgeben oder null, wenn nicht gefunden
+    }
+
+    // Funktion zur Ausgabe des gesamten Baums
+    public void printTree() {
+        printTree(root, "");
+    }
+
+    // Funktion zur Ausgabe des Baums unterhalb eines Knotens
+    public void printTree(Node currentNode, String prefix) {
+        if (currentNode == null) {
+            return; // nichts zu tun, wenn Knoten null ist
+        }
+        System.out.println(prefix + currentNode.getNumber()); // aktuellen Knoten ausgeben
+        printTree(currentNode.getLeft(), prefix + "L "); // linken Teilbaum ausgeben
+        printTree(currentNode.getRight(), prefix + "R "); // rechten Teilbaum ausgeben
+    }
+
+}
+
+
